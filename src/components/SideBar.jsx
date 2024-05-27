9ce6e7990c8569bc5c2567ae704b3af8ea31c1b2
Your Name <you@example.com>
2024-05-28 01:06:05 +0700

    edit

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -3,11 +3,28 @@ import { RiDashboardLine } from "react-icons/ri";
 import { AiOutlineAppstoreAdd } from "react-icons/ai";
 import { BsListCheck } from "react-icons/bs";
 import { MdOutlineImportantDevices } from "react-icons/md"
+import logo from "../assets/sidebar_log.avif"
+import user01 from "../assets/user-01.png"
 
 const SideBar = () => {
   return (
     <div className="w-full lg:w-64 bg-gray-900 shadow-lg  text-gray-200 min-h-screen fixed border-r-indigo-300">
-      <ul className="py-4 mt-20">
+      <div className="mt-10 flex items-center bg-gray-600">
+        <img src={logo} className="h-16 ml-3"></img>
+      <a class="btn btn-ghost text-2xl normal-case">Hotel Admin</a>
+      </div>
+
+      <div className="ml-5 mt-10 flex items-center">
+      <div class="btn btn-ghost btn-circle avatar">
+        <div class="w-15 rounded-full">
+          <img alt="Tailwind CSS Navbar component" src={user01} />
+        </div>
+      </div>
+      <a class="btn btn-ghost text-xl normal-case">Hi, Trung</a>
+      </div>
+
+      
+      <ul className="py-4 mt-5">
         <Link to="/">
           <li className="px-8 py-4 hover:bg-violet-600 flex items-center gap-3">
             <RiDashboardLine className="h-7 w-7" /> Home
@@ -32,6 +49,7 @@ const SideBar = () => {
           </li>
         </Link>
       </ul>
+      
     </div>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,8 +4,7 @@ import SideBar from "../components/SideBar";
 
 const Home = () => {
   return (
-    <div className="flex flex-col lg:flex-row lg:min-h-screen">
-      <Header />
+    <div className="flex flex-col lg:flex-row lg:min-h-screen bg-gray-800">``
       <SideBar />
       <Content />
     </div>
